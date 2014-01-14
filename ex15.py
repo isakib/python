e6b706d02e10b2b4d8be56ce52c74d7b6276a4e3
sakib <isakib@gmail.com>
2014-01-15 01:17:22 +0600

    new files

--- ex15.py
+++ ex15.py
@@ -1,17 +1,29 @@
+# from sys import argv
+# 
+# script, filename = argv
+# 
+# txt = open(filename)
+# 
+# print "here your file %r:" % filename
+# print txt.read()
+# 
+# print "type the filename again:"
+# file_again = raw_input("> ")
+# 
+# txt_again = open(file_again)
+# print txt_again.read()
+
 from sys import argv
 
 script, filename = argv
 
-txt = open(filename)
+text = open(filename)
 
 print "here your file %r:" % filename
-print txt.read()
+print text.read()
 
 print "type the filename again:"
 file_again = raw_input("> ")
 
-txt_again = open(file_again)
-dhd _n now
-txt_again = open(file_again)
-txt_again = open(okay_now_see)
-# try
+text_again = open(file_again)
+print text_again.read()
