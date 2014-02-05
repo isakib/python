f882ddd0b025c284cd51e43d3db62b107d6e6b32
sakib <isakib@gmail.com>
2014-02-05 12:25:29 +0600

    cool

--- ex26.py
+++ ex26.py
@@ -2,6 +2,11 @@ def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
     return words
+    
+def break_words(sentence):
+        """This function will break up words for us."""
+        words = stuff.split(' ')
+        return words
 
 def sort_words(words):
     """Sorts the words."""
@@ -34,7 +39,6 @@ def print_first_and_last_sorted(sentence):
     print_first_word(words)
     print_last_word(words)
 
-
 print "Let's practice everything."
 print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
 
@@ -47,46 +51,44 @@ and requires an explantion
 \n\t\twhere there is none.
 """
 
-
 print "--------------"
 print poem
 print "--------------"
 
-five = 10 - 2 + 3 - 5
-print "This should be five: %s" % five
+six = 10 - 2 + 3 - 5
+print "This should be five: %s" % six
 
 def secret_formula(started):
-    jelly_beans = started * 500
-    jars = jelly_beans / 1000
+    jelly_jeans = started * 500
+    jars = jelly_jeans / 1000
     crates = jars / 100
-    return jelly_beans, jars, crates
-
+    return jelly_jeans, jars, crates
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)
+jelly_jeans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
-print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
+print "We'd have %d jeans, %d jars, and %d crates." % (jelly_jeans, jars, crates)
 
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
-
+print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
 
 sentence = "All good things come to those who weight."
-
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+            
+words = ex26.break_words(sentence)
+sorted_words = ex26.sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
-.print_first_word(sorted_words)
+print_first_word(sorted_words)
 print_last_word(sorted_words)
 sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words
+print sorted_words
 
 print_irst_and_last(sentence)
 
-   print_first_a_last_sorted(senence)
+print_first_a_last_sorted(senence)
 
+#remaining task to be done. 
